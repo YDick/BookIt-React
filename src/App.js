ab5c28feea75b4a4a294254b03f191ea3cfbd1b2
YudiR <yudrose13@gmail.com>
2019-08-19 21:31:41 -0400

    added bootstrap and react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/googleMapsNpm/googleMaps'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-   <Map/>
+
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      data: {}};
+  }
+
+render() {
+    return (
+    <div>
+<Map/>
     </div>
-  );
+      )}
 }
 
 export default App;
