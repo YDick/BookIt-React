588601e3c0fe3b73ecaa56da13e8aabe7cd6080d
YudiR <yudrose13@gmail.com>
2019-08-29 22:19:41 -0400

    added react-router-bootstrap and icon

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import NavBar from './components/navBar/navBar'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/navBar/navBar";
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 
-
-
 class App extends React.Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
         <Router>
           <NavBar />
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup" exact component={Signup} />
+          <Switch>
+            <Route path="/home" exact component={Signup}/>
+            <Route  path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
+            
+          </Switch>
         </Router>
       </div>
     );
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,33 +1,63 @@
+
+
 import React from "react";
-import { Nav, Container, Navbar } from "react-bootstrap";
-import Link from 'react-router-dom'
+import { Nav, Container, Navbar, NavItem, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import Logo from './books.svg'
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
+  Crimson 
   render() {
     return (
-      <Navbar sticky="top" bg="primary" variant="dark">
-        <Navbar.Brand href="/">Navbar</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/">Book Clubs</Nav.Link>
-          <Nav.Link href="/">Profile</Nav.Link>
-          {/* if loged in: */}
+      <Navbar sticky="top" bg="primary" variant="dark" expand="lg">
+        <LinkContainer style={{ color: "#A9A9A9" }} to="/home">
+          <Navbar.Brand>
+              <img width="45"
+        height="45" src={Logo} className="d-inline-block align-top"/>
+          </Navbar.Brand>
+          {/* <img
+        src={Logo}
+        
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      /> */}
+        </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto ">
+          <LinkContainer  to="/home">
+            <Nav.Link active={false}>Home </Nav.Link>
+            {/* <Button> HOme</Button> */}
+          </LinkContainer>
+          {/* <Nav.Link href="/">Home</Nav.Link> */}
+          <LinkContainer to="/bookclubs">
+            <Nav.Link active={false}>Book Clubs</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/profile">
+            <Nav.Link active={false}>Profile </Nav.Link>
+          </LinkContainer>
+          {/* if logged out: */}
           {sessionStorage.length === 0 && (
-            <Nav.Link href="login">Sign In</Nav.Link>
+            <LinkContainer to="/login">
+              <Nav.Link active={false}>Log in</Nav.Link>
+            </LinkContainer>
           )}
           {sessionStorage.length === 0 && (
-            <Nav.Link href="signup">Sign Up</Nav.Link>
+            <LinkContainer to="/signup">
+              <Nav.Link active={false}>Sign up </Nav.Link>
+            </LinkContainer>
           )}
-          {/* if loged out: */}
+          {/* If logged in */}
           {sessionStorage.length === 1 && (
             <Nav.Link href="/">Sign Out </Nav.Link>
           )}
+
         </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
