5b80971037c4b258d6d470fdd99b259e3f64ca2c
YudiR <yudrose13@gmail.com>
2019-09-03 12:46:34 -0400

    made google map form into bootstrap form

--- src/components/mapForm/form.jsx
+++ src/components/mapForm/form.jsx
@@ -1,60 +1,56 @@
 import React from "react";
+import {Form, Button, FormGroup, FormControl, FormLabel, Col, Row} from "react-bootstrap";
 
-function Form(props) {
+function MapForm(props) {
 
   console.log("hello formmmm::::::::::::::::::");
   return (
     <React.Fragment>
-      <br /> <br />
-      <form >
-        <label>
-          <input
-            placeholder=" Street address "
+   
+      <Form>
+  <Row>
+    <Col>
+      <Form.Control placeholder=" Street address "
             type="text"
             name="street number"
             value={props.streetAddress}
             onChange={event => {
               props.dataChange({streetAddress:event.target.value})
-            }}
-          />
-        </label>
-
-        <label>
-          <input
-            placeholder="City"
+            }} />
+    </Col>
+    <Col>
+      <Form.Control placeholder="City"
             type="text"
             name="city"
             value={props.city}
             onChange={event => {
               props.dataChange({city:event.target.value})
-            }}
-          />
-        </label>
-        <label>
-          <input
-            placeholder="Postal Code"
+            }} />
+    </Col>
+    <Col>
+      <Form.Control   placeholder="Postal Code"
             type="text"
             name="postal code"
             value={props.postalCode}
             onChange={event => {
               props.dataChange({postalCode:event.target.value})
-            }}
-          />
-        </label>
-        
-        <button
-          value="Submit"
+            }} />
+    </Col>
+    <Col>
+    <Button  value="Submit"
           onClick={e => {
             e.preventDefault();
            
 
             props.submit();
-          }}
-        >
-          Submit
-        </button>
-      </form>
+          }}>
+    Submit
+  </Button>
+    </Col>
+  </Row>
+</Form>
+      
     </React.Fragment>
   );
 }
-export default Form;
+export default MapForm;
