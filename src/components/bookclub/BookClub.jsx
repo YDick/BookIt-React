16e869d9c6526574897ad35525ee8a213788d045
larisagranat <larisa.granat@gmail.com>
2019-09-10 23:18:09 -0400

    show page button Leave created

--- src/components/bookclub/BookClub.jsx
+++ src/components/bookclub/BookClub.jsx
@@ -13,12 +13,15 @@ class BookClub extends React.Component {
 
   componentDidMount() {
     let Luser = null;
-    if (currentUser() !== "NO TOKEN"){
-      currentUser().then(u=>u.json()).then(t=>{
-        console.log(t);
-        Luser = t.current_user;
-      });
+    if (currentUser() !== "NO TOKEN") {
+      currentUser()
+        .then(u => u.json())
+        .then(t => {
+          console.log(t);
+          Luser = t.current_user;
+        });
     }
+
     getBookClub(this.props.match.params.id)
       .then(e => e.json())
       .then(e => {
@@ -27,15 +30,14 @@ class BookClub extends React.Component {
           Promise: Promise
         });
 
-       googleMapsClient
-        .geocode({
-          address: `${e.book_club.address.address_line1} ${e.book_club.address.city}`
-        })
-        .asPromise()
-        .then(response => {
-          this.setState(
-            {
-              data: e ,
+        googleMapsClient
+          .geocode({
+            address: `${e.book_club.address.address_line1} ${e.book_club.address.city}`
+          })
+          .asPromise()
+          .then(response => {
+            this.setState({
+              data: e,
               lat: response.json.results[0].geometry.location.lat,
               lng: response.json.results[0].geometry.location.lng,
               Luser
@@ -48,6 +50,7 @@ class BookClub extends React.Component {
     let users = null;
     let admin = null;
     let message = "";
+    let btn = false;
     if (this.state.data !== null) {
       let lat = "";
       let lng = "";
@@ -55,46 +58,56 @@ class BookClub extends React.Component {
         users = this.state.data.users.map((e, i) => (
           <ListGroup.Item key={i}>{e.name}</ListGroup.Item>
         ));
+
+        this.state.data.users.forEach(element => {
+          if (element.email === this.state.Luser.email) {
+            btn = true;
+          }
+        });
       }
 
       if (this.state.data.admins.length > 0) {
         admin = this.state.data.admins.map((e, i) => (
           <ListGroup.Item key={i}>{e.name}</ListGroup.Item>
-        ));  
+        ));
       }
 
+      message = (
+        <React.Fragment>
+          <Card style={{ width: "28rem", margin: "auto" }}>
+            <Card.Img variant="top" src={this.state.data.book_club.image_url} />
+            <Card.Header>Welcome to</Card.Header>
+            <Card.Body>
+              <Card.Title>{this.state.data.book_club.name}</Card.Title>
+              <Card.Text>
+                {this.state.data.book_club.description ||
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis feugiat "}
+              </Card.Text>
 
-      message = <React.Fragment>
-        <Card style={{ width: "28rem", margin:'auto' }}>
-          <Card.Img variant="top" src={this.state.data.book_club.image_url} />
-          <Card.Header>Welcome to</Card.Header>
-          <Card.Body>
-            <Card.Title>{this.state.data.book_club.name}</Card.Title>
-            <Card.Text>
-              {this.state.data.book_club.description ||
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis feugiat "}
-            </Card.Text>
-
-            <div>
-              <div style={{ float: "left" }}>
-                {users !== null ? <h3>USERS::</h3> : null}
-                <ListGroup variant="flush">{users}</ListGroup>
+              <div>
+                <div style={{ float: "left" }}>
+                  {users !== null ? <h3>USERS::</h3> : null}
+                  <ListGroup variant="flush">{users}</ListGroup>
+                </div>
+                <div style={{ float: "right" }}>
+                  {admin !== null ? <h3>ADMIN::</h3> : null}
+                  <ListGroup variant="flush">{admin}</ListGroup>
+                </div>
               </div>
-              <div style={{ float: "right" }}>
-                {admin !== null ? <h3>ADMIN::</h3> : null}
-                <ListGroup variant="flush">{admin}</ListGroup>
-              </div>
-            </div>
-          </Card.Body>
+            </Card.Body>
 
-          <Button variant="primary">Go somewhere</Button>
+            {btn ? (
+              <Button variant="primary">Leave</Button>
+            ) : (
+              <Button variant="primary">Join</Button>
+            )}
 
-          <Card.Footer className="text-muted">
-            {`${this.state.data.book_club.address.address_line1} ${this.state.data.book_club.address.city} `}
-          </Card.Footer>
-        </Card>
+            <Card.Footer className="text-muted">
+              {`${this.state.data.book_club.address.address_line1} ${this.state.data.book_club.address.city} `}
+            </Card.Footer>
+          </Card>
 
-        <Map
+          <Map
             onClick={this.onMapClick}
             google={this.props.google}
             initialCenter={{
@@ -108,43 +121,37 @@ class BookClub extends React.Component {
             containerStyle={{
               width: "100%",
               height: "50px",
-              position: "relative",
+              position: "relative"
               // display: "flex",
               // justifyContent: "flexEnd"
             }}
             style={{
               height: "79vh",
               width: "150vh",
-              
+
               marginLeft: "auto",
-              marginRight: "auto",
+              marginRight: "auto"
               // display: "flex",
               // justifyContent: "center"
             }}
             zoom={15}
           >
-           <Marker
-                onClick={this.onMarkerClick}
-                position={{ lat: this.state.lat, lng: this.state.lng }}
-                title={this.state.data.book_club.name}
-                name={"hi"}
-              />
-          
+            <Marker
+              onClick={this.onMarkerClick}
+              position={{ lat: this.state.lat, lng: this.state.lng }}
+              title={this.state.data.book_club.name}
+              name={"hi"}
+            />
           </Map>
-        </React.Fragment>;
+        </React.Fragment>
+      );
     } else {
       message = <div>Loading......</div>;
     }
 
-    return (
-      <div >
-        {message}
-      </div>
-    );
+    return <div>{message}</div>;
   }
 }
-
-// export default BookClub;
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyC9YcNajcT4z5-USnDY-znyaf146i27YOU"
