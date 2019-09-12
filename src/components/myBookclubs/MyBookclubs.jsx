aed9944cf7520183fa788b0438efc2b022200222
YDick <ydick1998@gmail.com>
2019-09-12 14:54:10 -0400

    message if not signed in, layout for my clubs

--- src/components/myBookclubs/MyBookclubs.jsx
+++ src/components/myBookclubs/MyBookclubs.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable eqeqeq */
 import React, { Component } from "react";
-import {Card, CardGroup, Button} from "react-bootstrap";
+import {Card, CardGroup, Button, CardDeck, CardColumns} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 import './myBookclubs.css';
 
@@ -63,35 +63,47 @@ export default class MyBookclubs extends Component {
           .catch(error => console.error("Error:", error));
         }
 
+
+
           render()  {
+            console.log("my clubs:::: "+ this.state.myclubs+" clubs:::: "+this.state.clubs)
               return(
                 <div>
                     <h2>My Book Clubs:</h2>
                     <hr/>
+                    {
+                      sessionStorage.JWT ?
+                      <CardDeck>
+                      {this.state.myclubs.map((club,i) =>(
+                          <Card style={{ width: '18rem' }} key={i}>
+                          <Card.Img variant="top" src={club.image_url} />
+                          <Card.Body>
+                            <Card.Title>{club.name}</Card.Title>
+                            <Card.Text>
+                              {club.description}
+                            </Card.Text>
+                            <LinkContainer to={"/bookclubs/"+club.id}>
+                              <Button  class="button" variant="primary">Go To Club Page</Button>
+                            </LinkContainer>
+  
+                          </Card.Body>
+                          <Card.Footer className="text-muted">
+                                  Genre: {club.genre}
+                          </Card.Footer>
+                        </Card>
+                      ))}
+                      </CardDeck>
+                      :
+                      <p>Sign in to see your clubs!</p>
+                    }
+                   
 
-                    {this.state.myclubs.map((club,i) =>(
-                        <Card style={{ width: '18rem' }} key={i}>
-                        <Card.Img variant="top" src={club.image_url} />
-                        <Card.Body>
-                          <Card.Title>{club.name}</Card.Title>
-                          <Card.Text>
-                            {club.description}
-                          </Card.Text>
-                          <LinkContainer to={"/bookclubs/"+club.id}>
-                            <Button  class="button" variant="primary">Go To Club Page</Button>
-                          </LinkContainer>
-
-                        </Card.Body>
-                        <Card.Footer className="text-muted">
-                                Genre: {club.genre}
-                        </Card.Footer>
-                      </Card>
-                    ))}
+                    
 
                     <hr />
                     <h2>Browse Other Clubs:</h2>
                     <hr/>
-                    <CardGroup>
+                    <CardColumns>
                     {this.state.clubs.map((club,i) =>(
                         <Card style={{ width: '18rem' }} key={i+30}>
                         <Card.Img variant="top" src={club.image_url} />
@@ -109,7 +121,7 @@ export default class MyBookclubs extends Component {
                         </Card.Footer>
                       </Card>
                     ))}
-                    </ CardGroup>
+                    </CardColumns>
                 </div>
               )
                     }
